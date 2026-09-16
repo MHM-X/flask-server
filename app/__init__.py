0236718ed94eb65081d4dc3042840f44e71d8e0a
MHM-X <mahmoudemadmallouh@gmail.com>
2026-09-16 11:55:21 +0300

    added tag to the response header

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from app.config import Config
 from app.extensions import db
 from app.routes.items import items_bp
 from app.routes.orders import orders_bp
+import os
+
 
 
 def create_app():
@@ -15,5 +17,13 @@ def create_app():
     app.register_blueprint(items_bp)
 
     app.register_blueprint(orders_bp)
+
+    @app.after_request
+    def add_backend_header(response):
+        response.headers["X-Backend-Server"] = os.getenv(
+            "BACKEND_SERVER",
+            "unknown"
+        )
+        return response
 
     return app
